e079c6c3c9f0ad5137ae909e52cb34c2d8f86cfc
sgonzalezj96 <38997631+sgonzalezj96@users.noreply.github.com>
2019-08-23 09:29:06 -0500

    f

--- resources/views/notificacion/showadmin.blade.php
+++ resources/views/notificacion/showadmin.blade.php
@@ -1,74 +1,62 @@
-
-@extends('layouts.app')
-
-
+@extends('layouts.home')
+@section('title','Principal')
 @section('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">{{ __('Notificaciones') }}</div>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
 
-                    <div class="card-body">
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+                    
+@forelse ($notificaciones as $notificacion)
+<div class="post-content">
+    <div class="post-container">
+        <h1 align="center">Notificación</h1>
+        <div class="line-divider"></div>
+        <img src="{{asset('images/post-images/notificacion.jpg')}}" alt="user" class="profile-photo-md pull-left" />
+        <div class="post-detail">
+            <h5>Asunto : {{$notificacion->asunto}} </h5>
+            <div class="line-divider"></div>
+            <div class="post-text">
+                <p>
+                <td>{{$notificacion -> cuerpo}}</td>
+                </p>
+            </div>
+            <div class="post-text">
+                <p>
+                <td><h5>Atentamente: {{$notificacion -> email}}</h5></td>
+                </p>
+            </div>
+            <div class="line-divider"></div>
 
-                        <ul>
-                            @if ($errors->any())
-                                <div class ="alert alert-danger">
-                                    @foreach ($errors->all() as $error)
-                                        <li>{{ $error }}</li>
-                                    @endforeach
-                                </div>
-                            @endif
+        </div>
+    </div>
+</div>
+@empty
+<div class="post-content">
+    <div class="post-container">
+        <div class="post-detail">
+            <h5>No hay notificaciones </h5>
+            <div class="line-divider"></div>
+            <div class="post-text">
+                <p>
 
-                        </ul>
-                        @forelse ($notificaciones as $notificacion)
-                            <div class="post-content">
-                                <div class="post-container">
-                                    <div class="post-detail">
-                                        <h5>Asunto : {{$notificacion->asunto}} </h5>
-                                        <div class="line-divider"></div>
-                                        <div class="post-text">
-                                            <p>
-                                            <td>{{$notificacion -> cuerpo}}</td>
-                                            </p>
-                                        </div>
-                                        <div class="post-text">
-                                            <p>
-                                            <td>{{$notificacion -> email}}</td>
-                                            </p>
-                                        </div>
-                                        <div class="line-divider"></div>
+                </p>
+            </div>
+            <div class="line-divider"></div>
 
-                                    </div>
-                                </div>
-                            </div>
-                        @empty
-                            <div class="post-content">
-                                <div class="post-container">
-                                    <div class="post-detail">
-                                        <h5>No hay notificaciones </h5>
-                                        <div class="line-divider"></div>
-                                        <div class="post-text">
-                                            <p>
-
-                                            </p>
-                                        </div>
-                                        <div class="line-divider"></div>
-
-                                    </div>
-                                </div>
-                            </div>
-                        @endforelse
-
-
-                    </div>
+        </div>
+    </div>
+</div>
+@endforelse 
                 </div>
             </div>
         </div>
     </div>
+</div>
 @endsection
-
-
-
-
-
--- resources/views/notificacion/showuser.blade.php
+++ resources/views/notificacion/showuser.blade.php
@@ -1,30 +1,24 @@
-
-@extends('layouts.app')
-
-
+@extends('layouts.home')
+@section('title','Principal')
 @section('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">{{ __('Notificaciones') }}</div>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
 
-                    <div class="card-body">
-
-                        <ul>
-                            @if ($errors->any())
-                                <div class ="alert alert-danger">
-                                    @foreach ($errors->all() as $error)
-                                        <li>{{ $error }}</li>
-                                    @endforeach
-                                </div>
-                            @endif
-
-                        </ul>
-                        <h5> Notificaciones del Administrador</h5>
-                        @forelse ($notificaciones as $notificacion)
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+                    @forelse ($notificaciones as $notificacion)
                             <div class="post-content">
+                                    
                                 <div class="post-container">
+                                        <h1 align="center">Notificación</h1>
+                                        <div class="line-divider"></div>
+                                        <img src="{{asset('images/post-images/notificacion.jpg')}}" alt="user" class="profile-photo-md pull-left" />     
                                     <div class="post-detail">
                                         <h5>Asunto : {{$notificacion->asunto}} </h5>
                                         <div class="line-divider"></div>
@@ -56,14 +50,9 @@
                                 </div>
                             </div>
                         @endforelse
-
-
-                    </div>
                 </div>
             </div>
         </div>
     </div>
+</div>
 @endsection
-
-
-
